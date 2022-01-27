4b1a5aedf11d8bfd06d486da610997ce336671c5
Rodrigo Candido Faria <8796054+rfaria@users.noreply.github.com>
2022-01-26 21:35:01 -0300

    Update 001_List_Comprehensions.py

--- Python/02 - Basic Data Types/001_List_Comprehensions.py
+++ Python/02 - Basic Data Types/001_List_Comprehensions.py
@@ -1,2 +1,28 @@
 # Problem: https://www.hackerrank.com/challenges/list-comprehensions/problem
 
+def naive(x, y, z, n):
+    i = j = k = 0
+    all_permutations = []
+
+    for i in range(x+1):
+        for j in range(y+1):
+            for k in range(z+1):
+                if i+j+k != n:
+                    all_permutations.append([i, j, k])
+    
+    return all_permutations
+    
+    
+def optimized(x, y, z, n):
+    pass
+
+    
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+    result = naive(x,y,z,n)
+    
+    print(result)
