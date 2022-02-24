b5264fd9f16058f6568d81aff2c834a95bad2464
Rodrigo Candido Faria <8796054+rfaria@users.noreply.github.com>
2022-02-24 18:10:32 -0300

    Update 001_Hello_World.py

--- Python/01 - Introduction/001_Hello_World.py
+++ Python/01 - Introduction/001_Hello_World.py
@@ -1,3 +1,8 @@
+# Input Format
+# You do not need to read any input in this challenge.
+# Output Format
+# Print Hello, World! to stdout.
+
 if __name__ == '__main__':
     hello = "Hello, World!"
     print(hello)
